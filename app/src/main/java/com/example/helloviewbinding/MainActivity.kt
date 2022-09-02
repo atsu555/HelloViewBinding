38a36860ebffa9b19ab30594dc7e5b5e2f6a330e
kikkawa <wakaki555@kikkawaatsutaka.com>
2022-09-03 02:28:37 +0900

    viewBinding

--- app/src/main/java/com/example/helloviewbinding/MainActivity.kt
+++ app/src/main/java/com/example/helloviewbinding/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.helloviewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.helloviewbinding.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnCurrent.setOnClickListener {
+            binding.textResult.text = Date().toString()
+        }
     }
 }
